bb184b2023419a281bc13039724759f1e677db6d
Dana AlSiddig <danlsiddig@gmail.com>
2023-10-02 14:30:48 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const hqLocation = 42;
+    if (someValue > hqLocation) {
+        return (someValue - hqLocation)
+    }
+    else {
+        return (hqLocation - someValue)
+    }
+}
+
+function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+    const feetPerBlock = 264;
+    return (blocks * feetPerBlock)
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (destination > start) {
+        return ((destination - start) * 264)
+    }
+    else {
+        return ((start - destination) * 264)
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination)
+    if (distance < 400) {
+        return 0;
+    }
+    else if (distance >= 400 && distance <= 2000) {
+        return ((distance - 400) * 0.02)
+    }
+    else if (distance >= 2000 && distance <= 2500) {
+        return 25;
+    }
+    else if (distance >= 2500) {
+        return "cannot travel that far"
+    }
+}
